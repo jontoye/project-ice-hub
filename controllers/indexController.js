dbbd1d28469cde68457dc13e3109455a5805709e
Jonathan Toye <jontoye@gmail.com>
2022-03-13 14:02:20 -0400

    add date formatting on articles last updated

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const apiURL = 'https://api.sportsdata.io/v3/nhl/scores/json/News?key='
+const { DateTime } = require('luxon');
 
 const articles = require('../cache/articles.json');
 const nhlteams = require('../cache/nhlteams.json');
@@ -17,14 +18,19 @@ const nhlteams = require('../cache/nhlteams.json');
 // TEST VERSION - uses caches of api data
 exports.index = (req, res) => {
 
-    // Select articles to display and attach logo urls
+    // Select articles to display 
     const selections = articles.slice(0, 15).filter(article => article.Team != null);
     selections.forEach(article => {
+
+        // attach logo url
         nhlteams.find(team => {
             if (article.Team === team.Key) {
                 article.Logo = team.WikipediaLogoUrl;
             }
-        })
+        });
+
+        // format date
+        article.Updated = DateTime.fromISO(article.Updated).toRelative();
     });
 
 
